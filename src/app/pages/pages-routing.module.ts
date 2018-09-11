a023940f0889dcf8eb6903760e803d42a14f8212
mithatcak <mithatcancakmak@gmail.com>
2018-09-10 23:31:53 -0400

    initial commit

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -5,13 +5,15 @@ import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ECommerceComponent } from './e-commerce/e-commerce.component';
 import { NotFoundComponent } from './miscellaneous/not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
+
 
 const routes: Routes = [{
   path: '',
   component: PagesComponent,
   children: [{
-    path: 'dashboard',
-    component: ECommerceComponent,
+    path: 'home',
+    component: HomeComponent,
   }, {
     path: 'iot-dashboard',
     component: DashboardComponent,
